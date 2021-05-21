81752413785a49a8cbd90be1227351049c78297a
Davee Sok <daveesok@gmail.com>
2021-05-21 17:09:14 -0500

    added some configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: "gatsby practice",
+    title: 'gatsby practice',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: `/my-cool-sitemap.xml`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        /*
+        - basePath defaults to `/`
+        */
+        basePath: `/blog`,
+      },
+    },
+  ],
 };
