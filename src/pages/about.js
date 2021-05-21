f9ecd66465c587a68071c5d78f283921c1e942c8
Davee Sok <daveesok@gmail.com>
2021-05-21 15:52:40 -0500

    finished tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout.js';
+
+function About() {
+  return (
+    <Layout pageTitle='About page'>
+      <p> This about page is wrapped using a layout</p>
+    </Layout>
+  );
+}
+
+export default About;
